33573347538ecff19a3c5bff5a48bc5679ccc5fe
Ignacio Bardin <ilbardin@gmail.com>
2025-06-03 11:06:05 -0300

    agregar alerts.ts

---
+++ src/utils/alerts.ts
@@ -0,0 +1,48 @@
+import Swal from 'sweetalert2';
+import withReactContent from 'sweetalert2-react-content';
+
+const MySwal = withReactContent(Swal);
+
+export const showAlert = (
+    title: string,
+    icon: 'success' | 'error' | 'warning' | 'info' | 'question',
+    text?: string
+) => {
+    return MySwal.fire({
+        title,
+        icon,
+        html: text, // usarlo como html aunque el parámetro se llame text
+    });
+};
+
+export const showConfirm = async (
+    title: string,
+    text?: string,
+    confirmButtonText: string = 'Si',
+    cancelButtonText: string = 'No'
+) => {
+    const result = await MySwal.fire({
+        title,
+        html: text,
+        icon: 'question',
+        showCancelButton: true,
+        confirmButtonText,
+        cancelButtonText,
+        scrollbarPadding: false
+    });
+
+    return result.isConfirmed;
+};
+
+export const showLoading = (message: string = 'Cargando...') => {
+    void MySwal.fire({
+        title: message,
+        icon: 'info',
+        didOpen: () => {
+            Swal.showLoading();
+        },
+        allowOutsideClick: false,
+        allowEscapeKey: false,
+    });
+};
+
