1dcca78354b38c3ffc29fa73462dc3559534d4da
Ignacio Bardin <ilbardin@gmail.com>
2025-06-17 14:12:07 -0300

    mejoras estilo RegistroUsuario

--- src/pages/RegistroUsuario/RegistroUsuario.tsx
+++ src/pages/RegistroUsuario/RegistroUsuario.tsx
@@ -26,42 +26,6 @@ export const RegistroUsuario: React.FC = () => {
     const [showConfirmPassword, setShowConfirmPassword] = useState(false);
     const navigate = useNavigate();
 
-    const formFields = {
-        nombre: {
-            minLength: 4,
-            maxLength: 50,
-            placeholder: 'Nombre',
-        },
-        apellido: {
-            minLength: 4,
-            maxLength: 50,
-            placeholder: 'Apellido',
-        },
-        email: {
-            maxLength: 50,
-            placeholder: 'Email',
-        },
-        telefono: {
-            maxLength: 10,
-            placeholder: 'Teléfono',
-        },
-        username: {
-            minLength: 5,
-            maxLength: 20,
-            placeholder: 'Usuario',
-        },
-        password: {
-            minLength: 6,
-            maxLength: 20,
-            placeholder: 'Contraseña',
-        },
-        confirmPassword: {
-            minLength: 6,
-            maxLength: 20,
-            placeholder: 'Confirmar Contraseña',
-        },
-    };
-
     const handleShowPassword = (field: 'password' | 'confirmPassword') => {
         if (field === 'password') {
             setShowPassword((prev) => !prev);
@@ -91,7 +55,6 @@ export const RegistroUsuario: React.FC = () => {
                 username,
                 password,
             };
-            console.log(datosNuevoUsuario);
             showLoading('Creando cuenta...');
             await registrarUsuario(datosNuevoUsuario);
             Swal.close();
@@ -110,110 +73,119 @@ export const RegistroUsuario: React.FC = () => {
                 <div className={styles.wrapper}>
                     <form onSubmit={handleRegistroSubmit}>
                         <h1>Registro</h1>
-                        <div className={styles.inputBox}>
-                            <label htmlFor="nombre">Nombre:</label>
-                            <input
-                                type="text"
-                                id="nombre"
-                                minLength={formFields.nombre.minLength}
-                                maxLength={formFields.nombre.maxLength}
-                                value={nombre}
-                                onChange={(e) => setNombre(e.target.value)}
-                                required
-                            />
 
-                            <FaIdCard className={styles.icon}/>
-                        </div>
-                        <div className={styles.inputBox}>
-                            <label htmlFor="apellido">Apellido:</label>
-                            <input
-                                type="text"
-                                id="apellido"
-                                minLength={formFields.apellido.minLength}
-                                maxLength={formFields.apellido.maxLength}
-                                value={apellido}
-                                onChange={(e) => setApellido(e.target.value)}
-                                required
-                            />
-                            <FaIdCard className={styles.icon}/>
-                        </div>
-
-                        <div className={styles.inputBox}>
-                            <label htmlFor="email">Email:</label>
-                            <input
-                                type="email"
-                                id="email"
-                                maxLength={formFields.email.maxLength}
-                                value={email}
-                                onChange={(e) => setEmail(e.target.value)}
-                                required
-                            />
-                            <MdEmail className={styles.icon}/>
+                        <div className={styles.row}>
+                            <div className={styles.inputBox}>
+                                <label htmlFor="nombre">Nombre:</label>
+                                <FaIdCard className={styles.icon}/>
+                                <input
+                                    type="text"
+                                    id="nombre"
+                                    value={nombre}
+                                    onChange={(e) => setNombre(e.target.value)}
+                                    placeholder="Juan"
+                                    required
+                                />
+                            </div>
+                            <div className={styles.inputBox}>
+                                <label htmlFor="apellido">Apellido:</label>
+                                <input
+                                    type="text"
+                                    id="apellido"
+                                    value={apellido}
+                                    onChange={(e) => setApellido(e.target.value)}
+                                    placeholder="Pérez"
+                                    required
+                                />
+                                <FaIdCard className={styles.icon}/>
+                            </div>
                         </div>
 
-                        <div className={styles.inputBox}>
-                            <label htmlFor="telefono">Teléfono:</label>
-                            <input
-                                type="tel"
-                                id="telefono"
-                                maxLength={formFields.telefono.maxLength}
-                                value={telefono}
-                                onChange={(e) => setTelefono(e.target.value)}
-                                required
-                            />
-                            <BsTelephoneFill className={styles.icon}/>
+                        <div className={styles.row}>
+                            <div className={styles.inputBox}>
+                                <label htmlFor="email">Email:</label>
+                                <input
+                                    type="email"
+                                    id="email"
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)}
+                                    placeholder="mail@example.com"
+                                    required
+                                />
+                                <MdEmail className={styles.icon}/>
+                            </div>
+                            <div className={styles.inputBox}>
+                                <label htmlFor="telefono">Teléfono:</label>
+                                <input
+                                    type="tel"
+                                    id="telefono"
+                                    value={telefono}
+                                    onChange={(e) => setTelefono(e.target.value)}
+                                    placeholder="2612345678"
+                                    required
+                                />
+                                <BsTelephoneFill className={styles.icon}/>
+                            </div>
                         </div>
+
                         <div className={styles.inputBox}>
                             <label htmlFor="username">Usuario:</label>
                             <input
                                 type="text"
                                 id="username"
-                                maxLength={formFields.username.maxLength}
                                 value={username}
                                 onChange={(e) => setUsername(e.target.value)}
+                                placeholder="usuario"
                                 required
                             />
                             <FaUser className={styles.icon}/>
                         </div>
-                        <div className={styles.inputBox}>
-                            <label htmlFor="password">Contraseña:</label>
-                            <input
-                                type={showPassword ? 'text' : 'password'}
-                                id="password"
-                                minLength={formFields.password.minLength}
-                                maxLength={formFields.password.maxLength}
-                                value={password}
-                                onChange={(e) => setPassword(e.target.value)}
-                                required
-                            />
-                            <div
-                                className={`${styles.icon} ${styles.eyeIcon}`}
-                                onClick={() => handleShowPassword('password')}
-                            >
-                                {showPassword ? <AiFillEyeInvisible/> : <AiFillEye/>}
+
+                        <div className={styles.row}>
+                            <div className={styles.inputBox}>
+                                <label htmlFor="password">Contraseña:</label>
+                                <input
+                                    type={showPassword ? 'text' : 'password'}
+                                    id="password"
+                                    value={password}
+                                    onChange={(e) => setPassword(e.target.value)}
+                                    placeholder="********"
+                                    required
+                                />
+                                <div
+                                    className={`${styles.icon} ${styles.eyeIcon}`}
+                                    onClick={() => handleShowPassword('password')}
+                                >
+                                    {showPassword ? <AiFillEyeInvisible/> : <AiFillEye/>}
+                                </div>
+                            </div>
+
+                            <div className={styles.inputBox}>
+                                <label htmlFor="confirmar-password">Confirmar contraseña:</label>
+                                <input
+                                    type={showConfirmPassword ? 'text' : 'password'}
+                                    id="confirmar-password"
+                                    value={confirmPassword}
+                                    onChange={(e) => setConfirmPassword(e.target.value)}
+                                    placeholder="********"
+                                    required
+                                />
+                                <div
+                                    className={`${styles.icon} ${styles.eyeIcon}`}
+                                    onClick={() => handleShowPassword('confirmPassword')}
+                                >
+                                    {showConfirmPassword ? <AiFillEyeInvisible/> : <AiFillEye/>}
+                                </div>
                             </div>
                         </div>
 
-                        <div className={styles.inputBox}>
-                            <label htmlFor="confirmar-password">Confirmar contraseña:</label>
-                            <input
-                                type={showConfirmPassword ? 'text' : 'password'}
-                                id="confirmar-password"
-                                maxLength={formFields.confirmPassword.maxLength}
-                                value={confirmPassword}
-                                onChange={(e) => setConfirmPassword(e.target.value)}
-                                required
-                            />
-                            <div
-                                className={`${styles.icon} ${styles.eyeIcon}`}
-                                onClick={() => handleShowPassword('confirmPassword')}
-                            >
-                                {showConfirmPassword ? <AiFillEyeInvisible/> : <AiFillEye/>}
-                            </div>
-                        </div>
-                        <button type="submit" className={styles.btn}>Registrarse</button>
+                        <button type="submit" className={styles.botonRegistro}>
+                            Registrarse
+                        </button>
                         <div className={styles.loginLink}>
-                            <p>¿Ya tienes cuenta? <Link to={ROUTES.LOGIN}>Inicia sesión</Link></p>
+                            <p>
+                                ¿Ya tienes cuenta? <Link to={ROUTES.LOGIN}>Inicia sesión</Link>
+                            </p>
                         </div>
                     </form>
                 </div>
