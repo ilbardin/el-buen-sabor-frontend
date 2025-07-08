8b5df3ff7014311d5e28e13a6f712454b77b4fed
Ignacio Bardin <ilbardin@gmail.com>
2025-07-08 15:29:21 -0300

    ajuste export AuthProvider

--- src/context/auth/AuthProvider.tsx
+++ src/context/auth/AuthProvider.tsx
@@ -1,8 +1,12 @@
-import React, {useEffect, useState} from 'react';
+import {type ReactNode, useEffect, useState} from 'react';
 import {AuthContext} from './authContext.ts';
 import type {UserData, Usuario} from '../../models/usuario/usuario.ts';
 
-export const AuthProvider: React.FC = ({children}) => {
+interface AuthProviderProps {
+    children: ReactNode;
+}
+
+export const AuthProvider = ({children}: AuthProviderProps) => {
     const [tokenJwt, setJwt] = useState<string | null>(null);
     const [jwtExpirationDate, setExpirationDate] = useState<Date | null>(null);
     const [usuario, setUsuario] = useState<Usuario | null>(null);
