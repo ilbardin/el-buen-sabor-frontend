576ef998974bc1294a372c14070571235bbb476d
Ignacio Bardin <ilbardin@gmail.com>
2025-07-15 17:47:25 -0300

    quitar DuracionCarritoAlert de App

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,12 @@ import {BrowserRouter} from 'react-router-dom';
 import {AuthProvider} from './context/auth/AuthProvider.tsx';
 import Router from './Router';
 import {CartProvider} from "./context/carrito/CarritoContext.tsx";
-import DuracionCarritoAlert from "./components/DuracionCarritoAlert/DuracionCarritoAlert.tsx";
 
 function App() {
     return (
         <AuthProvider>
             <BrowserRouter>
                 <CartProvider>
-                    <DuracionCarritoAlert/>
                     <Router/>
                 </CartProvider>
             </BrowserRouter>
