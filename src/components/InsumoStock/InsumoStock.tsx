ed16a0ab33398c0fd91f765579ebc0d4bced50cf
FJFFH <franfioreh123@gmail.com>
2025-07-16 11:06:22 -0300

    Funcion para el manejo de la cantidad de stock

--- src/components/InsumoStock/InsumoStock.tsx
+++ src/components/InsumoStock/InsumoStock.tsx
@@ -1,6 +1,23 @@
-import React from "react";
+import { useState } from "react";
 import type { StockInsumo } from "../../models/stockInsumo.ts";
 export function InsumoStock(props: { stockInsumo: StockInsumo }) {
+
+  const [stockActual, setStockActual] = useState(props.stockInsumo.cantidadActual);
+
+
+  function manejarStock(numero: number) {
+    const nuevoStock = stockActual + numero;
+    if (nuevoStock >= 0) {
+      setStockActual(nuevoStock);
+      // Aquí podrías agregar lógica para actualizar el stock en el backend
+      console.log(`Nuevo stock para ${props.stockInsumo.denominacion}: ${nuevoStock}`);
+    } else {
+      console.warn("No se puede reducir el stock por debajo de cero");
+    }
+
+
+  }
+
   return (
     <tr>
       <td>{props.stockInsumo.idInsumo}</td>
@@ -9,10 +26,13 @@ export function InsumoStock(props: { stockInsumo: StockInsumo }) {
       <td>{props.stockInsumo.cantidadMaxima}</td>
       <td>{props.stockInsumo.unidadMedida}</td>
       <td>
-        <button>-</button> 
-        <input type="text" value={props.stockInsumo.cantidadActual}/> 
-        <button>+</button>
+        <button onClick={() => manejarStock(-1)} >-</button> 
+        <input type="text" value={stockActual}/> 
+        <button onClick={() => manejarStock(1)}>+</button>
       </td>
+      <td>
+        <button>❌</button>
+        <button>✔️</button></td>
     </tr>
   );
 }
