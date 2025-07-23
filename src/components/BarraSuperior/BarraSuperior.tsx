3e61fe05b7080ffd26f5e4ce0235d5f312201782
FJFFH <franfioreh123@gmail.com>
2025-07-23 17:33:06 -0300

    Integrar los menus en la barra superior

--- src/components/BarraSuperior/BarraSuperior.tsx
+++ src/components/BarraSuperior/BarraSuperior.tsx
@@ -1,56 +1,71 @@
-import {Link, Outlet, useNavigate} from 'react-router-dom';
-import {useAuth} from '../../context/auth/useAuth.ts';
-import {UserRole} from "../../models/usuario/userRoles.ts";
-import {ROUTES} from "../../constants/routes.ts";
-import styles from './BarraSuperior.module.css';
-import {BiSolidLogOut} from "react-icons/bi";
+import { Link, Outlet, useNavigate } from "react-router-dom";
+import { useAuth } from "../../context/auth/useAuth.ts";
+import { UserRole } from "../../models/usuario/userRoles.ts";
+import { ROUTES } from "../../constants/routes.ts";
+import styles from "./BarraSuperior.module.css";
+import { BiSolidLogOut } from "react-icons/bi";
+import GestionEmpresa from "../../pages/GestionEmpresa/GestionEmpresa.tsx";
+
 
 export const BarraSuperior = () => {
-    const {logout, setIsLoggingOut, usuario} = useAuth();
-    const navigate = useNavigate();
+  const { logout, setIsLoggingOut, usuario } = useAuth();
+  const navigate = useNavigate();
 
-    const handleLogout = () => {
-        setIsLoggingOut(true);
-        logout();
-        navigate(ROUTES.LOGIN);
-    };
+  const handleLogout = () => {
+    setIsLoggingOut(true);
+    logout();
+    navigate(ROUTES.LOGIN);
+  };
 
-    return (
+    if (location.pathname === "/gestion-empresa") {
+    return ( 
         <div>
-            <nav className={styles.topbar}>
-                <ul>
-                    <li>
-                        <Link to={ROUTES.HOME}>Home</Link>
-                    </li>
-                    {(usuario?.rol === UserRole.Admin || usuario?.rol === UserRole.Cliente) && (
-                        <>
-                            <li>
-                                <Link to={ROUTES.PRODUCTOS}>Productos</Link>
-                            </li>
-                        </>
-                    )}
-                </ul>
-                {usuario && (
-                    <>
-                        <p className={styles.userInfo}>
-                            Estás logueado como usuario: <strong>{usuario.rol}</strong>
-                        </p>
-                        <button className={styles.logoutButton} onClick={handleLogout}>
-                            <BiSolidLogOut size={18} style={{marginRight: 2}}/>
-                            Cerrar sesión
-                        </button>
-                    </>
-                )}
-                {!usuario && (
-                    <button className={styles.loginButton} onClick={() => navigate('/login')}>
-                        Iniciar sesión
-                    </button>
-                )}
-            </nav>
-
-            <main className={styles.pageContent}>
-                <Outlet/>
-            </main>
-        </div>
+        <GestionEmpresa />
+      </div>
     );
+  }
+  return (
+    <div>
+      <nav className={styles.topbar}>
+        <ul>
+          <li>
+            <Link to={ROUTES.HOME}>Home</Link>
+          </li>
+          {(usuario?.rol === UserRole.Admin ||
+            usuario?.rol === UserRole.Cliente) && (
+            <>
+              <li>
+                <Link to={ROUTES.PRODUCTOS}>Productos</Link>
+              </li>
+
+              <li>
+                <Link to={ROUTES.GESTION_EMPRESA}>Gestion de Empresa</Link>
+              </li>
+              
+            </>
+          )}
+        </ul>
+        {usuario && (
+          <>
+            <button className={styles.logoutButton} onClick={handleLogout}>
+              <BiSolidLogOut size={18} style={{ marginRight: 2 }} />
+              Cerrar sesión
+            </button>
+          </>
+        )}
+        {!usuario && (
+          <button
+            className={styles.loginButton}
+            onClick={() => navigate("/login")}
+          >
+            Iniciar sesión
+          </button>
+        )}
+      </nav>
+
+      <main className={styles.pageContent}>
+        <Outlet />
+      </main>
+    </div>
+  );
 };
--- src/pages/GestionEmpresa/GestionEmpresa.tsx
+++ src/pages/GestionEmpresa/GestionEmpresa.tsx
@@ -3,35 +3,78 @@ import { ProductosABM } from "../ProductosABM/ProductosABM";
 import { StockABM } from "../StockABM/StockABM";
 import { IngredientesABM } from "../IngredientesABM/IngredientesABM";
 import styles from "./GestionEmpresa.module.css";
+import { ROUTES } from "../../constants/routes";
+import { Link, useNavigate } from "react-router-dom";
+import { UserRole } from "../../models/usuario/userRoles";
+import { useAuth } from "../../context/auth/useAuth";
+import { BiSolidLogOut } from "react-icons/bi";
+//import styles from "../components/BarraSuperior/BarraSuperior.module.css";
 
 export default function GestionEmpresa() {
   const [componenteActivo, setComponenteActivo] = useState("manofacturados");
+  const { logout, setIsLoggingOut, usuario } = useAuth();
+   const navigate = useNavigate();
 
+     const handleLogout = () => {
+       setIsLoggingOut(true);
+       logout();
+       navigate(ROUTES.LOGIN);
+     };
   return (
     <div>
-      <div className={styles.selectorContainer}>
-        <div className={styles.botonesWrapper}>
-          <div
+      <nav className={styles.topbar}>
+        <ul>
+          <li>
+            <Link to={ROUTES.HOME}>Home</Link>
+          </li>
+          {(usuario?.rol === UserRole.Admin ||
+            usuario?.rol === UserRole.Cliente) && (
+            <>
+              <li>
+                <Link to={ROUTES.PRODUCTOS}>Productos</Link>
+              </li>
+            </>
+          )}
+          <li
             className={styles.botonSelector}
             onClick={() => setComponenteActivo("manofacturados")}
           >
             Productos Manofacturados
-          </div>
+          </li>
 
-          <div
+          <li
             className={styles.botonSelector}
             onClick={() => setComponenteActivo("insumos")}
           >
             Productos Insumos
-          </div>
-          <div
+          </li>
+          <li
             className={styles.botonSelector}
             onClick={() => setComponenteActivo("stock")}
           >
             Control de Stock
-          </div>
-        </div>
-      </div>
+          </li>
+        </ul>
+
+        {usuario && (
+          <>
+            <button className={styles.logoutButton} onClick={handleLogout}>
+              <BiSolidLogOut size={18} style={{ marginRight: 2 }} />
+              Cerrar sesión
+            </button>
+          </>
+        )}
+        {!usuario && (
+          <button
+            className={styles.loginButton}
+            onClick={() => navigate("/login")}
+          >
+            Iniciar sesión
+          </button>
+        )}
+      </nav>
+
+      
 
       <div>
         {componenteActivo === "manofacturados" && <ProductosABM />}
