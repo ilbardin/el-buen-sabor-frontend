381bee70b06ef772cc43cfd5b1133591a77a7efc
Ignacio Bardin <ilbardin@gmail.com>
2025-08-12 16:20:31 -0300

    mejoras de logica y manejo de errores

--- src/components/PromocionItem/PromocionItem.tsx
+++ src/components/PromocionItem/PromocionItem.tsx
@@ -1,44 +1,53 @@
-import type { Promocion } from "../../models/promocion";
-import { eliminarPromocion } from "../../services/promocionService";
+import type {Promocion} from "../../models/promocion";
+import {eliminarPromocion} from "../../services/promocionService";
 import style from "./PromocionItem.module.css";
+import {showConfirm, showLoading} from "../../utils/alerts.ts";
 
 export function PromocionItem(props: {
-  promocion: Promocion;
-  onEditar: (promo: Promocion) => void;
+    promocion: Promocion;
+    onEditar: (promo: Promocion) => void;
+    onEliminar: () => void;
 }) {
-  const deletePromocion = async (id: number) => {
-    await eliminarPromocion(id);
-  };
 
-  return (
-    <>
-      <tr className={style.fila}>
-        <td>{props.promocion.id!}</td>
-        <td>{props.promocion.denominacion}</td>
-        <td>{props.promocion.descuento}</td>
-        <td>{props.promocion.fechaDesde}</td>
-        <td>{props.promocion.fechaHasta}</td>
-        <td>
-          <button
-            className={style.botonEditar}
-            onClick={() => {
-              props.onEditar(props.promocion);
-            }}
-          >
-            Modificar
-          </button>
-          <button
-            className={style.boatonEliminar}
-            onClick={() => {
-              if (props.promocion.id !== undefined) {
-                deletePromocion(props.promocion.id);
-              }
-            }}
-          >
-            Eliminar
-          </button>
-        </td>
-      </tr>
-    </>
-  );
+    const deletePromocion = async (id: number) => {
+        const confirmacion: boolean = await showConfirm("Eliminar promoción", "¿Estás seguro de que deseas eliminar esta promoción?");
+
+        if (confirmacion) {
+            showLoading();
+            await eliminarPromocion(id);
+            props.onEliminar();
+        }
+    };
+
+    return (
+        <>
+            <tr className={style.fila}>
+                <td>{props.promocion.id!}</td>
+                <td>{props.promocion.denominacion}</td>
+                <td>{props.promocion.descuento}</td>
+                <td>{props.promocion.fechaDesde}</td>
+                <td>{props.promocion.fechaHasta}</td>
+                <td>
+                    <button
+                        className={style.botonEditar}
+                        onClick={() => {
+                            props.onEditar(props.promocion);
+                        }}
+                    >
+                        Modificar
+                    </button>
+                    <button
+                        className={style.boatonEliminar}
+                        onClick={async () => {
+                            if (props.promocion.id !== undefined) {
+                                await deletePromocion(props.promocion.id);
+                            }
+                        }}
+                    >
+                        Eliminar
+                    </button>
+                </td>
+            </tr>
+        </>
+    );
 }
--- src/pages/PromocionesABM/PromocionesABM.tsx
+++ src/pages/PromocionesABM/PromocionesABM.tsx
@@ -1,88 +1,112 @@
-import { useState, useEffect } from "react";
+import React, {useState, useEffect} from "react";
 import styles from "./PromocionesABM.module.css";
-import type { Promocion } from "../../models/promocion.ts";
-import { PromocionItem } from "../../components/PromocionItem/PromocionItem.tsx";
-import { getPromociones } from "../../services/promocionService.ts";
-import { FormularioPromocion } from "../../components/FormularioPromocion/FormularioPromocion.tsx";
+import type {Promocion} from "../../models/promocion.ts";
+import {PromocionItem} from "../../components/PromocionItem/PromocionItem.tsx";
+import {getPromociones} from "../../services/promocionService.ts";
+import {FormularioPromocion} from "../../components/FormularioPromocion/FormularioPromocion.tsx";
+import {showAlert, showLoading} from "../../utils/alerts.ts";
+import {handleNetworkError} from "../../utils/errorHandler.ts";
+import Swal from "sweetalert2";
 
 export const PromocionesABM = () => {
-  const [promociones, setPromociones] = useState<Promocion[]>([]);
-  const [mostrarModal, setMostrarModal] = useState(false);
-  const [busqueda, setBusqueda] = useState("");
+    const [promociones, setPromociones] = useState<Promocion[]>([]);
+    const [mostrarModal, setMostrarModal] = useState(false);
+    const [busqueda, setBusqueda] = useState("");
 
-  const [promocionAEditar, setPromocionAEditar] = useState<Promocion | null>(
-    null
-  );
+    const [promocionAEditar, setPromocionAEditar] = useState<Promocion | null>(
+        null
+    );
 
-  //! CARGA DE PROMOCIONES
-  const cargarPromociones = async () => {
-    try {
-      const data = await getPromociones();
-      console.log("Promociones:", data);
-      setPromociones(data);
-    } catch (error) {
-      console.error("Error al cargar las promociones:", error);
-    }
-  };
+    //! CARGA DE PROMOCIONES
+    const cargarPromociones = async () => {
+        showLoading("Cargando promociones...");
 
-  useEffect(() => {
-    void cargarPromociones();
-  }, []);
+        try {
+            const data = await getPromociones();
+            console.log("Promociones:", data);
+            setPromociones(data);
 
-  //! BUSCADOR
-  const handleChangeBusqueda = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setBusqueda(e.target.value);
-  };
+            Swal.close();
+        } catch (error: any) {
+            console.error("Error al cargar las promociones:", error);
 
-  const resultados = promociones.filter((promo) =>
-    promo.denominacion.toLowerCase().includes(busqueda.toLowerCase())
-  );
+            if (await handleNetworkError(error)) {
+                return;
+            }
 
-  return (
-    <div className={styles.container}>
-      {mostrarModal && (
-        <FormularioPromocion
-          onClose={() => {
-            setMostrarModal(false);
-            setPromocionAEditar(null);
-            void cargarPromociones();
-          }}
-          promocionAEditar={promocionAEditar} //? revisar
-        />
-      )}
+            await showAlert("Error", "error", "Error al cargar lista de promociones.");
+        }
+    };
 
-      <h1 className={styles.titulo}>Gestión de Promociones</h1>
-      <button className={styles.boton} onClick={() => {setMostrarModal(true); setPromocionAEditar(null);}}>
-        Cargar Promoción
-      </button>
+    useEffect(() => {
+        void cargarPromociones();
+    }, []);
 
-      <div className={styles.filtrosContainer}>
-        <input
-          type="text"
-          placeholder="Buscar"
-          onChange={handleChangeBusqueda}
-          className={styles.filtroInput}
-          value={busqueda}
-        />
-      </div>
+    //! BUSCADOR
+    const handleChangeBusqueda = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setBusqueda(e.target.value);
+    };
 
-      <table className={styles.tabla}>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Denominación</th>
-            <th>Descuento</th>
-            <th>Fecha Desde</th>
-            <th>Fecha Hasta</th>
-            <th>Botones</th>
-          </tr>
-        </thead>
-        <tbody>
-          {resultados.map((promo) => (
-            <PromocionItem key={promo.id} promocion={promo} onEditar={(promo) => {setPromocionAEditar(promo); setMostrarModal(true)}}/>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  );
+    const resultados = promociones.filter((promo) =>
+        promo.denominacion.toLowerCase().includes(busqueda.toLowerCase())
+    );
+
+    return (
+        <div className={styles.container}>
+            {mostrarModal && (
+                <FormularioPromocion
+                    onClose={() => {
+                        setMostrarModal(false);
+                        setPromocionAEditar(null);
+                        void cargarPromociones();
+                    }}
+                    promocionAEditar={promocionAEditar} //? revisar
+                />
+            )}
+
+            <h1 className={styles.titulo}>Gestión de Promociones</h1>
+            <button className={styles.boton} onClick={() => {
+                setMostrarModal(true);
+                setPromocionAEditar(null);
+            }}>
+                Cargar Promoción
+            </button>
+
+            <div className={styles.filtrosContainer}>
+                <input
+                    type="text"
+                    placeholder="Buscar"
+                    onChange={handleChangeBusqueda}
+                    className={styles.filtroInput}
+                    value={busqueda}
+                />
+            </div>
+
+            <table className={styles.tabla}>
+                <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Denominación</th>
+                    <th>Descuento</th>
+                    <th>Fecha Desde</th>
+                    <th>Fecha Hasta</th>
+                    <th>Botones</th>
+                </tr>
+                </thead>
+                <tbody>
+                {resultados.map((promo) => (
+                    <PromocionItem
+                        key={promo.id}
+                        promocion={promo}
+                        onEditar={(promo) => {
+                            setPromocionAEditar(promo);
+                            setMostrarModal(true)
+                        }}
+                        onEliminar={cargarPromociones}
+                    />
+                ))}
+                </tbody>
+            </table>
+        </div>
+    );
 };
