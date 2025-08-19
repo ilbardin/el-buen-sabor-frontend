2c5f5f543e33f4bd10539abe8702a1ba1e65adce
Ignacio Bardin <ilbardin@gmail.com>
2025-08-19 16:06:54 -0300

    ajuste variable interfaz

--- src/models/pedidoRequest.ts
+++ src/models/pedidoRequest.ts
@@ -10,7 +10,7 @@ export interface PedidoRequest {
 
 export interface DetallePedido {
     cantidad: number;
-    subTotal: number;
+    subtotal: number;
     articuloManufacturado?: { id: number };
     articuloInsumo?: { id: number };
 }
