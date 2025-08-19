51a2a3e9ba8a2f71c280f561226f0f1c3c8b4e1f
Ignacio Bardin <ilbardin@gmail.com>
2025-08-19 12:07:38 -0300

    ajustar tipo pedido

--- src/services/mercadoPagoService.ts
+++ src/services/mercadoPagoService.ts
@@ -1,10 +1,10 @@
 import axiosInstance from "../api/axiosInstance.ts";
-import type {PedidoMercadoPago} from "../models/pedidoRequest.ts";
+import type {PedidoRequest} from "../models/pedidoRequest.ts";
 
 interface PreferenceMP {
     id: string;
     init_point: string;
 }
 
-export const crearPeticionMP = (pedido: PedidoMercadoPago) =>
+export const crearPeticionMP = (pedido: PedidoRequest) =>
     axiosInstance.post<PreferenceMP>('/checkout', pedido).then(res => res.data);
