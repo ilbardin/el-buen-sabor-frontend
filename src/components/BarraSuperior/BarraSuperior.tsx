6686b6231bff738338329b2678261bff0dd1369d
FJFFH <franfioreh123@gmail.com>
2025-08-22 19:30:12 -0300

    fix

--- src/components/BarraSuperior/BarraSuperior.tsx
+++ src/components/BarraSuperior/BarraSuperior.tsx
@@ -1,56 +1,63 @@
-
-import { Link, Outlet, useNavigate } from "react-router-dom";
-import { useAuth } from "../../context/auth/useAuth.ts";
-import { ROUTES } from "../../constants/routes.ts";
-import styles from "./BarraSuperior.module.css";
-import { BiSolidLogOut } from "react-icons/bi";
 import { SlArrowDown } from "react-icons/sl";
 import GestionEmpresa from "../../pages/GestionEmpresa/GestionEmpresa.tsx";
 import { useEffect, useState } from "react";
 import { getSucursal } from "../../services/sucursalService.ts";
 import type { Sucursal } from "../../models/sucursal.ts";
 import { useSucursalStore } from "../Sucursal/SucursalStore.tsx";
-import {Link, Outlet, useNavigate} from 'react-router-dom';
-import {useAuth} from '../../context/auth/useAuth.ts';
-import {UserRole} from "../../models/usuario/userRoles.ts";
-import {ROUTES} from "../../constants/routes.ts";
-import styles from './BarraSuperior.module.css';
-import {BiSolidLogOut} from "react-icons/bi";
-import {alertaCarrito, existeCarrito} from "../../utils/funcionesReutilizables.ts";
+import { Link, Outlet, useNavigate } from "react-router-dom";
+import { useAuth } from "../../context/auth/useAuth.ts";
+import { UserRole } from "../../models/usuario/userRoles.ts";
+import { ROUTES } from "../../constants/routes.ts";
+import styles from "./BarraSuperior.module.css";
+import { BiSolidLogOut } from "react-icons/bi";
+import {
+  alertaCarrito,
+  existeCarrito,
+} from "../../utils/funcionesReutilizables.ts";
 
 export const BarraSuperior = () => {
   const { logout, setIsLoggingOut, usuario } = useAuth();
   const navigate = useNavigate();
   const idSucursal = useSucursalStore((state) => state.idSucursal);
-  const sucursalNombre = useSucursalStore((state) => state.denominacionSucursal)
+  const sucursalNombre = useSucursalStore(
+    (state) => state.denominacionSucursal
+  );
   const setIdSucursal = useSucursalStore((state) => state.setIdSucursal);
   const setDenominacion = useSucursalStore((state) => state.setDenominacion);
 
-    const handleUserLogout = async () => {
-        const performLogout = () => {
-            setIsLoggingOut(true);
-            logout();
-            navigate(ROUTES.HOME, {replace: true});
-        };
-
-        if (existeCarrito()) {
-            const confirmacion = await alertaCarrito();
-
-            if (!confirmacion) {
-                return;
-            }
-        }
-
-        performLogout();
-        // hack para cambiar el estado de setIsLoggingOut una vez haya finalizado la navegacion
-        // setTimeout(() => setIsLoggingOut(false), 500);
+  const handleLogout = () => {
+    setIsLoggingOut(true);
+    logout();
+    navigate(ROUTES.LOGIN);
+  };
+  const handleUserLogout = async () => {
+    const performLogout = () => {
+      setIsLoggingOut(true);
+      logout();
+      navigate(ROUTES.HOME, { replace: true });
     };
+
+    if (existeCarrito()) {
+      const confirmacion = await alertaCarrito();
+
+      if (!confirmacion) {
+        return;
+      }
+    }
+
+    performLogout();
+    // hack para cambiar el estado de setIsLoggingOut una vez haya finalizado la navegacion
+    // setTimeout(() => setIsLoggingOut(false), 500);
+  };
 
   const cambioIdSucursal = (id: number, denominacion: string) => {
     setIdSucursal(id);
-    setDenominacion(denominacion)
-    console.log("Sucursal cambiada a ID:", useSucursalStore.getState().idSucursal);
-  }
+    setDenominacion(denominacion);
+    console.log(
+      "Sucursal cambiada a ID:",
+      useSucursalStore.getState().idSucursal
+    );
+  };
 
   const [sucursales, setSucursales] = useState<Sucursal[]>([]);
 
@@ -65,11 +72,9 @@ export const BarraSuperior = () => {
 
   if (location.pathname === "/gestion-empresa") {
     return (
-
       <div>
         <GestionEmpresa />
       </div>
-
     );
   }
   return (
@@ -127,8 +132,14 @@ export const BarraSuperior = () => {
             </div>
             <ul className={styles.dropdownMenu}>
               {sucursales.map((sucursal) => (
-                <li className={styles.items}
-                  onClick={() => cambioIdSucursal(Number(sucursal.id), String(sucursal.nombre))}
+                <li
+                  className={styles.items}
+                  onClick={() =>
+                    cambioIdSucursal(
+                      Number(sucursal.id),
+                      String(sucursal.nombre)
+                    )
+                  }
                   key={sucursal.id}
                 >
                   {" "}
