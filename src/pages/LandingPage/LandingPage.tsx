7733200e0177522368b5ac68ff789b12ca9c58ab
Ignacio Bardin <ilbardin@gmail.com>
2025-09-04 21:04:16 -0300

    ajustes estilos landing

--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -74,13 +74,11 @@ export const LandingPage = () => {
     }, [setIsLoggingOut]);
 
     return (
-        <div className={styles.container}>
+        <div className={styles.containerLanding}>
             <NavbarCliente
                 usuario={usuario}
                 navLinks={[
-                    {label: "Menú", to: ROUTES.PRODUCTOS, requiresAuth: true},
-                    {label: "Nuestros especiales", to: "/especiales"},
-                    {label: "Sucursales", to: "/sucursales"},
+                    {label: "MENÚ", to: ROUTES.PRODUCTOS, requiresAuth: true}
                 ]}
                 cartOptions={{
                     showCart,
@@ -124,18 +122,6 @@ export const LandingPage = () => {
                     <img src={imagenPizza} alt="Pizza" className={styles.image}/>
                 </div>
             </main>
-
-            <footer className={styles.footer}>
-                <div className={styles.dishOfTheWeek}>
-                    <h3>LO MEJOR DE LA SEMANA</h3>
-                    <p>Lo más vendido</p>
-                    <div className={styles.dishPrices}>
-                        <div>$9.44 <br/>Empanadas de carne</div>
-                        <div>$12.48 <br/>Lomito Simple</div>
-                        <div>$20 <br/>Pizza muzzarella</div>
-                    </div>
-                </div>
-            </footer>
         </div>
     );
 };
