3a3f499d6849a2f09db429227d1d6004fded0d42
Ignacio Bardin <ilbardin@gmail.com>
2025-09-08 15:55:46 -0300

    agregar componente ControlEmpleadosPage

---
+++ src/pages/ControlEmpleadosPage/ControlEmpleadosPage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ControlEmpleadosPage: React.FC = () => {
+    return (
+        <div>
+            <h1>Control de Empleados</h1>
+        </div>
+    );
+}
+
+export default ControlEmpleadosPage;
