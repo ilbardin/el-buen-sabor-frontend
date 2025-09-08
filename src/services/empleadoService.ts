cd878e7040984fc1cccf4d1a9ceb4b352971d3cb
Ignacio Bardin <ilbardin@gmail.com>
2025-09-08 16:03:34 -0300

    fix axiosInstance empleadoService

--- src/services/empleadoService.ts
+++ src/services/empleadoService.ts
@@ -1,11 +1,11 @@
-import axios from "axios";
 import type {Empleado} from "../models/usuario/empleado.ts";
+import axiosInstance from "../api/axiosInstance.ts";
 
 const URL_EMPLEADOS = "/empleados";
 
 export async function getEmpleados(): Promise<Empleado[]> {
     try {
-        const response = await axios.get<Empleado[]>(`${URL_EMPLEADOS}`);
+        const response = await axiosInstance.get<Empleado[]>(`${URL_EMPLEADOS}`);
         return response.data;
     } catch (error) {
         console.error("Error al obtener los empleados:", error);
