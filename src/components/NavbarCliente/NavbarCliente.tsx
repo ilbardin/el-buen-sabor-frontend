3166653fbca8e1c37a7f55617dc090d4fa9e782b
FJFFH <franfioreh123@gmail.com>
2025-09-08 20:54:47 -0300

    boton admin

--- src/components/NavbarCliente/NavbarCliente.tsx
+++ src/components/NavbarCliente/NavbarCliente.tsx
@@ -1,183 +1,200 @@
-import React, {type RefObject, useContext, useEffect, useState} from "react";
-import {Link} from "react-router-dom";
-import {FaShoppingCart, FaUser} from "react-icons/fa";
+import React, { type RefObject, useContext, useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { FaShoppingCart, FaUser } from "react-icons/fa";
 import styles from "./NavbarCliente.module.css";
 import CarritoCard from "../CarritoCard/CarritoCard.tsx";
 import UserCard from "../UserCard/UserCard.tsx";
-import {CartContext} from "../../context/carrito/cartContext.ts";
-import {useAuthHandlers} from "../../hooks/useAuthHandlers.ts";
-import {useOutsideClick} from "../../hooks/useOutsideClick.ts";
+import { CartContext } from "../../context/carrito/cartContext.ts";
+import { useAuthHandlers } from "../../hooks/useAuthHandlers.ts";
+import { useOutsideClick } from "../../hooks/useOutsideClick.ts";
 import Logo from "../Logo/Logo.tsx";
+import { UserRole } from "../../models/usuario/userRoles.ts";
+import { MdAdminPanelSettings } from "react-icons/md";
 
 export interface NavLink {
-    label: string;
-    to: string;
-    requiresAuth?: boolean;
-    icon?: React.ReactNode;
+  label: string;
+  to: string;
+  requiresAuth?: boolean;
+  icon?: React.ReactNode;
 }
 
 export interface CartOptions {
-    showCart: boolean;
-    isCartClosing: boolean;
-    cartPosition: { top: number; left: number };
-    toggleCart: () => void;
-    handleHideCart: () => void;
-    cartRef: RefObject<HTMLDivElement | null>;
-    cartIconRef: RefObject<HTMLSpanElement | null>;
+  showCart: boolean;
+  isCartClosing: boolean;
+  cartPosition: { top: number; left: number };
+  toggleCart: () => void;
+  handleHideCart: () => void;
+  cartRef: RefObject<HTMLDivElement | null>;
+  cartIconRef: RefObject<HTMLSpanElement | null>;
 }
 
 export interface NavbarProps {
-    usuario: { nombre: string; apellido: string } | null;
-    navLinks: NavLink[];
-    cartOptions?: CartOptions;
+  usuario: { nombre: string; apellido: string; rol?: UserRole } | null;
+  navLinks: NavLink[];
+  cartOptions?: CartOptions;
 
-    loginRef: RefObject<HTMLDivElement | null>;
-    userIconRef: RefObject<HTMLSpanElement | null>;
+  loginRef: RefObject<HTMLDivElement | null>;
+  userIconRef: RefObject<HTMLSpanElement | null>;
 }
 
 const NavbarCliente: React.FC<NavbarProps> = ({
-                                                  usuario,
-                                                  navLinks,
-                                                  cartOptions,
-                                                  loginRef,
-                                                  userIconRef,
-                                              }) => {
-    const {cart} = useContext(CartContext);
-    const existeCarrito = cart.length > 0;
+  usuario,
+  navLinks,
+  cartOptions,
+  loginRef,
+  userIconRef,
+}) => {
+  console.log("Datos del usuario en Navbar:", usuario);
+  const { cart } = useContext(CartContext);
+  const existeCarrito = cart.length > 0;
 
-    const [showLogin, setShowLogin] = useState(false);
-    const [isClosing, setIsClosing] = useState(false);
-    const [loginCardPosition, setLoginCardPosition] = useState({top: 0, left: 0});
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
+  const [showLogin, setShowLogin] = useState(false);
+  const [isClosing, setIsClosing] = useState(false);
+  const [loginCardPosition, setLoginCardPosition] = useState({
+    top: 0,
+    left: 0,
+  });
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-    const {handleLogin, handleUserLogout} = useAuthHandlers(username, password);
-
-    const toggleLogin = () => {
-        if (showLogin) {
-            setIsClosing(true);
-            setTimeout(() => {
-                setShowLogin(false);
-                setIsClosing(false);
-            }, 300);
-        } else {
-            setShowLogin(true);
-        }
-    };
-
-    const handleHideLogin = () => {
+  const { handleLogin, handleUserLogout } = useAuthHandlers(username, password);
+  const toggleLogin = () => {
+    if (showLogin) {
+      setIsClosing(true);
+      setTimeout(() => {
         setShowLogin(false);
         setIsClosing(false);
-    };
+      }, 300);
+    } else {
+      setShowLogin(true);
+    }
+  };
 
-    const getInitials = (name: string, surname: string) => {
-        const parts = `${name} ${surname}`.trim().split(" ");
-        return parts.map((p) => p[0].toUpperCase()).join("").slice(0, 2);
-    };
+  const handleHideLogin = () => {
+    setShowLogin(false);
+    setIsClosing(false);
+  };
 
-    useEffect(() => {
-        if ((showLogin || isClosing) && userIconRef.current) {
-            const rect = userIconRef.current.getBoundingClientRect();
-            const top = rect.bottom + window.scrollY + 8;
-            const left = rect.right + window.scrollX - 320;
-            setLoginCardPosition({top, left});
-        }
-    }, [showLogin, isClosing, userIconRef]);
+  const getInitials = (name: string, surname: string) => {
+    const parts = `${name} ${surname}`.trim().split(" ");
+    return parts
+      .map((p) => p[0].toUpperCase())
+      .join("")
+      .slice(0, 2);
+  };
 
-    useOutsideClick({
-        refs: [loginRef],
-        enabled: showLogin,
-        onOutsideClick: () => {
-            if (showLogin) toggleLogin();
-        },
-    });
+  useEffect(() => {
+    if ((showLogin || isClosing) && userIconRef.current) {
+      const rect = userIconRef.current.getBoundingClientRect();
+      const top = rect.bottom + window.scrollY + 8;
+      const left = rect.right + window.scrollX - 320;
+      setLoginCardPosition({ top, left });
+    }
+  }, [showLogin, isClosing, userIconRef]);
 
-    useOutsideClick({
-        refs: cartOptions ? [cartOptions.cartRef, cartOptions.cartIconRef] : [],
-        enabled: !!cartOptions && cartOptions.showCart,
-        onOutsideClick: cartOptions?.handleHideCart || (() => {
-        }),
-    });
+  useOutsideClick({
+    refs: [loginRef],
+    enabled: showLogin,
+    onOutsideClick: () => {
+      if (showLogin) toggleLogin();
+    },
+  });
 
-    return (
-        <header className={styles.navbar}>
-            <Logo/>
-            <div className={styles.navbarRight}>
-                <nav className={styles.navLinks}>
-                    {navLinks.map(({label, to, requiresAuth, icon}, idx) => {
-                        if (requiresAuth && !usuario) return null;
-                        return (
-                            <Link key={idx} to={to} className={styles.navItem}>
-                                {icon && <span className={styles.navIcon}>{icon}</span>}
-                                {label}
-                            </Link>
-                        );
-                    })}
-                </nav>
+  useOutsideClick({
+    refs: cartOptions ? [cartOptions.cartRef, cartOptions.cartIconRef] : [],
+    enabled: !!cartOptions && cartOptions.showCart,
+    onOutsideClick: cartOptions?.handleHideCart || (() => {}),
+  });
 
-                <div className={styles.actions}>
-                    {usuario && cartOptions && (
-                        <>
-            <span
+  return (
+    <header className={styles.navbar}>
+      <Logo />
+      <div className={styles.navbarRight}>
+        <nav style={{marginRight: "20px"}} className={styles.navLinks}>
+          {usuario?.rol === "ADMIN" && (
+            <Link to="/productos-abm" className={styles.navItem}>
+              <span className={styles.navIcon}>
+                <MdAdminPanelSettings />
+              </span>
+              ADMIN
+            </Link>
+          )}
+        </nav>
+        <nav className={styles.navLinks}>
+          {navLinks.map(({ label, to, requiresAuth, icon }, idx) => {
+            if (requiresAuth && !usuario) return null;
+            return (
+              <Link key={idx} to={to} className={styles.navItem}>
+                {icon && <span className={styles.navIcon}>{icon}</span>}
+                {label}
+              </Link>
+            );
+          })}
+        </nav>
+
+        <div className={styles.actions}>
+          {usuario && cartOptions && (
+            <>
+              <span
                 className={styles.icon}
                 onClick={cartOptions.toggleCart}
                 ref={cartOptions.cartIconRef}
-                style={{position: "relative"}}
-            >
-              <FaShoppingCart/>
+                style={{ position: "relative" }}
+              >
+                <FaShoppingCart />
                 {existeCarrito && (
-                    <span
-                        className={styles.cartDot}
-                        style={{
-                            position: "absolute",
-                            bottom: 0,
-                            right: 0,
-                            width: "10px",
-                            height: "10px",
-                            borderRadius: "50%",
-                            backgroundColor: "red",
-                        }}
-                    />
+                  <span
+                    className={styles.cartDot}
+                    style={{
+                      position: "absolute",
+                      bottom: 0,
+                      right: 0,
+                      width: "10px",
+                      height: "10px",
+                      borderRadius: "50%",
+                      backgroundColor: "red",
+                    }}
+                  />
                 )}
-            </span>
+              </span>
 
-                            <CarritoCard
-                                showCart={cartOptions.showCart}
-                                isClosing={cartOptions.isCartClosing}
-                                position={cartOptions.cartPosition}
-                                onHide={cartOptions.handleHideCart}
-                                ref={cartOptions.cartRef}
-                            />
-                        </>
-                    )}
-
-                    <span className={styles.icon} onClick={toggleLogin} ref={userIconRef}>
-          {usuario ? (
-              <div className={styles.userCircle}>
-                  {getInitials(usuario.nombre, usuario.apellido)}
-              </div>
-          ) : (
-              <FaUser/>
+              <CarritoCard
+                showCart={cartOptions.showCart}
+                isClosing={cartOptions.isCartClosing}
+                position={cartOptions.cartPosition}
+                onHide={cartOptions.handleHideCart}
+                ref={cartOptions.cartRef}
+              />
+            </>
           )}
-        </span>
 
-                    <UserCard
-                        showLogin={showLogin}
-                        isClosing={isClosing}
-                        loginCardPosition={loginCardPosition}
-                        handleLogin={handleLogin}
-                        username={username}
-                        password={password}
-                        setUsername={setUsername}
-                        setPassword={setPassword}
-                        onLogout={handleUserLogout}
-                        onHide={handleHideLogin}
-                        ref={loginRef}
-                    />
-                </div>
-            </div>
-        </header>
-    );
+          <span className={styles.icon} onClick={toggleLogin} ref={userIconRef}>
+            {usuario ? (
+              <div className={styles.userCircle}>
+                {getInitials(usuario.nombre, usuario.apellido)}
+              </div>
+            ) : (
+              <FaUser />
+            )}
+          </span>
+
+          <UserCard
+            showLogin={showLogin}
+            isClosing={isClosing}
+            loginCardPosition={loginCardPosition}
+            handleLogin={handleLogin}
+            username={username}
+            password={password}
+            setUsername={setUsername}
+            setPassword={setPassword}
+            onLogout={handleUserLogout}
+            onHide={handleHideLogin}
+            ref={loginRef}
+          />
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default NavbarCliente;
