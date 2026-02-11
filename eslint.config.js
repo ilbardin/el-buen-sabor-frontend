fdb96e0ec12fe97ab7b26f61faed6279198c672a
Ignacio <ilbardin@gmail.com>
2026-02-11 15:26:23 -0300

    deshabilitar regla react-hooks/set-state-in-effect en eslint

--- eslint.config.js
+++ eslint.config.js
@@ -23,7 +23,8 @@ export default tseslint.config(
                 'warn',
                 {allowConstantExport: true},
             ],
-            "@typescript-eslint/no-explicit-any": "warn"
+            "@typescript-eslint/no-explicit-any": "warn",
+            'react-hooks/set-state-in-effect': 'off'
         },
     },
 )
