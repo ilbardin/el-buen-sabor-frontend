a6e2ce36679f07e8ea9f3c9c75f8f03beb38b6a2
Ignacio <ilbardin@gmail.com>
2026-03-11 19:07:21 -0300

    ajustes SucursalStore

--- src/components/Sucursal/SucursalStore.tsx
+++ src/components/Sucursal/SucursalStore.tsx
@@ -1,23 +1,23 @@
-import { create } from "zustand";
-import { persist } from "zustand/middleware";
+import {create} from "zustand";
+import {persist} from "zustand/middleware";
 
 type SucursalStatedsf = {
-  idSucursal: number | null;
+    idSucursal: number | null;
     denominacionSucursal: string | null;
-  setIdSucursal: (id: number) => void;
-  setDenominacion: (denominacion: string) => void;
+    setIdSucursal: (id: number) => void;
+    setDenominacion: (denominacion: string) => void;
 };
 
 export const useSucursalStore = create(
-  persist<SucursalStatedsf>(
-    (set) => ({
-      denominacionSucursal: "Seleccione una Sucursal",
-      idSucursal: 1,
-      setIdSucursal: (id) => set({ idSucursal: id }),
-      setDenominacion: (denominacion) => set({ denominacionSucursal: denominacion }),
-    }),
-    {
-      name: "sucursal-storage",
-    }
-  )
+    persist<SucursalStatedsf>(
+        (set) => ({
+            denominacionSucursal: "Seleccione una Sucursal",
+            idSucursal: 1,
+            setIdSucursal: (id) => set({idSucursal: id}),
+            setDenominacion: (denominacion) => set({denominacionSucursal: denominacion}),
+        }),
+        {
+            name: "sucursal-storage",
+        }
+    )
 );
